d343200806f92abf7b6e0d553551a1f88ee69a32
Ranjeet Singh Rawat <ranjeet843507@gmail.com>
2023-04-02 13:48:01 +0530

    News Component and news item component init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import NavBar from './Components/NavBar'
+import News from './Components/News'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-<NavBar/>      </div>
+<NavBar/>   
+<News/> 
+  </div>
     )
   }
 }
